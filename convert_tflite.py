717a1e8f15060ad158abd2f0789b26c6b9464735
bingabh <binn23102003@gmail.com>
2025-06-09 16:12:04 +0700

    fitx1

--- convert_tflite.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-model = tf.keras.models.load_model("MobileNet_RGB-2506.h5")
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-tflite_model = converter.convert()
-open("model.tflite", "wb").write(tflite_model)
-print("Đã chuyển sang model.tflite")
